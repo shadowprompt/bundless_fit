bd4c885a278becd83e1dbcf53e433f3b38347a30
shadowprompt <huixiong.cn@gmail.com>
2023-07-29 11:58:07 +0800

    [fix]暂存

--- app.js
+++ app.js
@@ -3,18 +3,29 @@ const path = require('path')
 const multer = require('multer');
 const fs = require("fs");
 const cors = require('cors');
-const { dLog } = require('@daozhao/utils');
+const ejs = require('ejs');
+const { dLog, nodeStore } = require('@daozhao/utils');
 const {mkdirsSync} = require("./node/tools");
 
 mkdirsSync('/tmp/fit_upload_temp');
 const upload = multer({ dest: '/tmp/fit_upload_temp/' });
 const huaweiHandler = require('./node/huaweiHandler');
 const zeppHandler = require('./node/zeppHandler');
-const app = express()
+const localStorage = nodeStore('../localStorage/bundless_fit');
+const app = express();
 
 app.use(cors())
-
+app.set('views', __dirname + '/build'); // 修改ejs模板查找文件夹
+app.engine('html', ejs.__express); // 修改默认的模板后缀为.html
 app.use(express.static('/tmp'));
+app.get('/', (req, res) => {
+  let prevList = localStorage.getItem('list') || '[]';
+  prevList = JSON.parse(prevList);
+  const data = {
+    count:  prevList.length + 50,
+  };
+  res.render('index.html', data);
+});
 app.use(express.static(path.join(__dirname, '../bundless_fit/build'))); // 直接读取bundless_fit web打包后的文件夹
 app.use(express.static(path.join(__dirname, './build'))); // 直接读取bundless_fit web打包后的文件夹
 // Routes
@@ -24,6 +35,22 @@ app.get(`/`, (req, res) => {
 
 
 app.post('/upload', upload.array('zip_file', 1), function(req,res){
+  const address = req.body.address;
+  const type = req.body.type;
+  const ts = Date.now();
+  const fileName = 'fit_' + ts;
+
+  let prevList = localStorage.getItem('list') || '[]';
+  prevList = JSON.parse(prevList);
+  localStorage.setItem('list', JSON.stringify([
+    ...prevList,
+    {
+      address,
+      type,
+      fileName,
+      ts,
+    }
+  ]));
   mkdirsSync('/tmp/fit_upload_temp');
 
   for(let i in req.files){
@@ -36,14 +63,13 @@ app.post('/upload', upload.array('zip_file', 1), function(req,res){
       const originalName = file.originalname;
       const list = originalName.split('.');
       const ext = list[list.length - 1];
-      const fileName = 'fit_' + Date.now();
       const baseFilePath = `/tmp/fit_upload`;
       mkdirsSync(baseFilePath);
       const targetPath= `${baseFilePath}/${fileName}.${ext}`;
       //使用同步方式重命名一个文件
       fs.renameSync(file.path, targetPath);
-      dLog('successfully rename the file to ', file.path, targetPath, req.body.type);
-      const handler = req.body.type === 'huawei' ? huaweiHandler : zeppHandler;
+      dLog('successfully rename the file to ', file.path, targetPath, type);
+      const handler = type === 'huawei' ? huaweiHandler : zeppHandler;
       return handler.preCheck(targetPath).then(result => {
         const baseUrl = `https://fit.bundless.cn/fit_upload/${fileName}`;
 
@@ -51,8 +77,9 @@ app.post('/upload', upload.array('zip_file', 1), function(req,res){
           handler.parser({
             data: {
               requestBody: {
-                address: req.body.address,
                 info: {
+                  address,
+                  type,
                   baseFilePath,
                   filePath: targetPath,
                   baseUrl,
