35ecfca1f33a0864f195952e0e5172ae3343b221
hxiong <hxiong@ctrip.com>
2024-01-03 15:07:27 +0800

    [fix]调整内容

--- node/huaweiDataDesc.js
+++ node/huaweiDataDesc.js
@@ -1,8 +1,8 @@
 // tp = xx 含义
-{
+export const dataMap = {
   '经纬度': 'lbs',
   '心率': 'tp',
-  '步频Cadence': ['s-r/2', 'pad', 'cad'],
+  '步频': ['s-r/2', 'pad', 'cad'], // Cadence
   '海拔': 'alti',
   '配速信息': 'p-m',
   '实时速度': 'rs',
