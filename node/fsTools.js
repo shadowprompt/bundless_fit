c6f7d9b64ed238d7f75236e57b7af8bbd02378e3
hxiong <hxiong@ctrip.com>
2024-01-07 10:33:33 +0800

    [fix]优化xiaomi解析csv

--- node/fsTools.js
+++ node/fsTools.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const papaparse = require('papaparse');
 const csv = require('csv-parser');
 function readCsv(filePath){
   const results = [];
@@ -13,26 +12,18 @@ function readCsv(filePath){
           return;
         }
         // 判断当前是顺序还是倒序
-        if (mode === 'desc') {
-          results.unshift(data);
-        } else if (mode === 'asc') {
-          results.push(data);
-        } else {
+        if (!mode) {
           const prev = results[i - 1];
           if (prev) {
+            // 只有能显示判断顺序还是倒序才标记mode
             if (prev.Time > data.Time) {
               mode = 'desc';
-              results.unshift(data);
             } else if (prev.Time < data.Time) {
               mode = 'asc';
-              results.push(data);
-            } else { // 相等时无法判断，留给下一次再判断
-              results.push(data);
             }
-          } else {
-            results.push(data);
           }
         }
+        results.push(data);
 
         if (i % 50000 === 0) {
           console.log('i ~ ', i, mode);
@@ -40,28 +31,13 @@ function readCsv(filePath){
         i++;
       })
       .on('end', () => {
-        console.log('end ~ ', results.length);
-        resolve(results);
+        console.log('end ~ ', results.length, mode);
+        if (mode === 'desc') {
+          resolve(results.reverse());
+        } else {
+          resolve(results);
+        }
       });
-  });
-
-  const file = fs.readFileSync(filePath);
-  papaparse.parse(file, {
-    "delimiter":",",
-    "header":false,
-    "dynamicTyping":false,
-    "skipEmptyLines":false,
-    "preview":0,
-    "encoding":"",
-    "worker":false,
-    "comments":"",
-    "download":false,
-    step: function(results, parser) {
-      console.log('step', results);
-    },
-    complete: function(results) {
-      console.log('complete', results);
-    }
   });
 }
 
